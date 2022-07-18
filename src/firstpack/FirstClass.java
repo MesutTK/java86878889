d248e91cd39cbc3f2f81e10d6ace8eff322abf68
MesutTK <109423786+MesutTK@users.noreply.github.com>
2022-07-18 12:32:01 +0300

    last from remote

--- src/firstpack/FirstClass.java
+++ src/firstpack/FirstClass.java
@@ -4,6 +4,12 @@ public class FirstClass {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println("This is new update on local master branch");
+        
+        
+        
+        
+        
+        
         System.out.println("remote master update");
     }
 }
